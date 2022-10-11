9f9b7ad4ef7b0491991a37c96cd67869ab68d39a
SerhatCelikk <90610606+SerhatCelikk@users.noreply.github.com>
2022-10-11 19:47:35 +0300

    added components

---
+++ components/homeComponents/home3.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home3 = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">Home3!</h1>
+        </div>
+    );
+}
+
+export default Home3;
