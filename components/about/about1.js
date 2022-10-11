2fa156ce411e0e2dcdd244800f50a9dd589deb12
SerhatCelikk <90610606+SerhatCelikk@users.noreply.github.com>
2022-10-12 01:24:36 +0300

    about page updated

--- components/about/about1.js
+++ components/about/about1.js
@@ -1,8 +1,34 @@
 import React from "react";
-
+import styles from './about1.module.css';
 
 const About1 = () => {
-    return(<p>xxx</p>
+    return( <div className={styles.backgroundImage}>
+        <div class="flex text-center text-white pt-8  max-w-6xl">
+          <div class="flex-1 mt-8 ">
+            <h4 className="text-3xl">THE RISE OF THE</h4>
+            <h2 className="text-6xl ">STORY BASED NFT</h2>
+            <p className="text-xl opacity-80 my-5 mx-20">
+              Over the last few months, we’ve seen a rise of the “Story NFT” meta
+              as more projects try to incorporate richer narratives and world
+              building into their collections.
+            </p>
+            <p className="text-xl opacity-80 ">
+              But with the exception of a few projects, we haven’t seen many
+              collections that have gone all the way to telling a cohesive
+              narrative with actual plots and character development...
+            </p>
+          </div>
+          <div class="img flex-1">
+            <div className="z-0">
+              <img
+                className={styles.backgroundCharacter}
+                src="https://streetmachine.oss-us-west-1.aliyuncs.com/images/about/img.png"
+                layout="fill"
+              ></img>
+            </div>
+          </div>
+        </div>
+      </div>
     );
 };
 
