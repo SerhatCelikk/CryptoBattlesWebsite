55c72f31e2b773355c1237cd63d1fb126b6f6515
BesteKaratepe <106274276+BesteKaratepe@users.noreply.github.com>
2022-10-12 16:11:59 +0300

    Gallery page updates

--- components/gallery/gallery1.js
+++ components/gallery/gallery1.js
@@ -6,14 +6,22 @@ const Gallery1 = () => {
     <div >
       <div class="grid-rows-5 gap-0 place-content-center w-screen">
         <div className={styles.backgroundImage}>
-          <div >
+          <div class="h-max" >
             <p className={styles.title}>Gallery</p>
           </div>
         </div>
-        <div className={styles.backgroundcolor}>xxx</div>
-        <div className={styles.backgroundcolor}>xxx</div>
-        <div className={styles.backgroundcolor}>xxx</div>
-        <div className={styles.backgroundcolor}>xxx</div>
+        <div className={styles.backgroundcolor}><div class="h-96" >
+            
+          </div></div>
+        <div className={styles.backgroundcolor}><div class="h-96" >
+            
+          </div></div>
+        <div className={styles.backgroundcolor}><div class="h-96" >
+            
+          </div></div>
+        <div className={styles.backgroundcolor}><div class="h-96" >
+            
+          </div></div>
       </div>
     </div>
   );
