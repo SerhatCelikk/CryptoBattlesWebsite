85762ed1f17ea1e656eb35350087d81108eb4cf3
BesteKaratepe <106274276+BesteKaratepe@users.noreply.github.com>
2022-10-14 10:52:25 +0300

    Gallery page background update

--- pages/gallery.js
+++ pages/gallery.js
@@ -2,11 +2,12 @@ import React from "react";
 import StickyHeader from '../components/header/stickyheader';
 import Gallery1 from '../components/gallery/gallery1';
 import Gallery2 from '../components/gallery/gallery2';
+import styles from "../styles/gallery.module.css";
 
 
 export default function Gallery() {
   return (
-    <div>
+    <div className={styles.gallery} >
         <StickyHeader />
         <Gallery1 />
         <Gallery2 />
