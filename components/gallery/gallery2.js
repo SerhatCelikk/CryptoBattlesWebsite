52196cd0b7bbc89a69b70c68d55bdb4ba2906953
BesteKaratepe <106274276+BesteKaratepe@users.noreply.github.com>
2022-10-17 22:13:35 +0300

    Gallery page update

--- components/gallery/gallery2.js
+++ components/gallery/gallery2.js
@@ -7,13 +7,13 @@ const Gallery2 = () => {
     <div className={styles.main}>
     <div className={styles.combine}>
         <div className={styles.area1} >
-            <img lassName={styles.img1}src="https://streetmachine.oss-us-west-1.aliyuncs.com/images/gallery/img.png"/>
+            <img className={styles.img1}src="https://streetmachine.oss-us-west-1.aliyuncs.com/images/gallery/img.png"/>
         </div>
 
         <div className={styles.area2} >
             <div className={styles.gallery2}>
-                <img lassName={styles.img2} src="https://streetmachine.oss-us-west-1.aliyuncs.com/images/gallery/02.jpg" />
-                <img lassName={styles.img2} src="https://streetmachine.oss-us-west-1.aliyuncs.com/images/gallery/03.jpg"/>
+                <img className={styles.img2} src="https://streetmachine.oss-us-west-1.aliyuncs.com/images/gallery/02.jpg" />
+                <img className={styles.img22} src="https://streetmachine.oss-us-west-1.aliyuncs.com/images/gallery/03.jpg"/>
             </div>
 
             <div className={styles.gallery3}>
@@ -24,10 +24,10 @@ const Gallery2 = () => {
                   <li className={styles.li3}>
                     <img className={styles.img3} src="https://streetmachine.oss-us-west-1.aliyuncs.com/images/gallery/A2.png" />
                   </li>
-                  <li className={styles.li3}>
+                  <li className={styles.li33}>
                     <img className={styles.img3} src="https://streetmachine.oss-us-west-1.aliyuncs.com/images/gallery/A3.png" />
                   </li>
-                  <li className={styles.li3}>
+                  <li className={styles.li333}>
                     <img className={styles.img3} src="https://streetmachine.oss-us-west-1.aliyuncs.com/images/gallery/A4.png" />
                   </li>
                 </ul>
@@ -46,7 +46,7 @@ const Gallery2 = () => {
                   <li className={styles.li4}>
                     <img className={styles.img4} src="https://streetmachine.oss-us-west-1.aliyuncs.com/images/gallery/B3.png" />
                   </li>
-                  <li className={styles.li4}>
+                  <li className={styles.li44}>
                     <img className={styles.img4} src="https://streetmachine.oss-us-west-1.aliyuncs.com/images/gallery/B4.png" />
                   </li>
 
@@ -59,7 +59,7 @@ const Gallery2 = () => {
                   <li className={styles.li4} >
                     <img className={styles.img4} src="https://streetmachine.oss-us-west-1.aliyuncs.com/images/gallery/B7.png" />
                   </li>
-                  <li className={styles.li4}>
+                  <li className={styles.li44}>
                     <img className={styles.img4} src="https://streetmachine.oss-us-west-1.aliyuncs.com/images/gallery/B8.png" />
                   </li>
                 </ul>
