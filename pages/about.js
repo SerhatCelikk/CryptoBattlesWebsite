4847429da0dbdce443b1a73b1fe6c76c050ba873
SerhatCelikk <90610606+SerhatCelikk@users.noreply.github.com>
2022-10-22 20:19:07 +0300

    updated header

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,16 @@
 import React from "react";
 import StickyHeader from '../components/header/stickyheader';
 import About1 from '../components/about/about1';
+import Head from 'next/head'
 import Footer from "../components/footer/footer";
 export default function About() {
   return (
-    <div>
+    <div><Head>
+    <title>About - Cyber Battles</title>
+    <meta name="description" content="Created by Cyber Battles" />
+    <link rel="icon" href="/favicon.ico" />
+            
+  </Head>
       <StickyHeader />
       <About1 />
       
--- pages/gallery.js
+++ pages/gallery.js
@@ -4,13 +4,19 @@ import Gallery1 from '../components/gallery/gallery1';
 import Gallery2 from '../components/gallery/gallery2';
 import styles from "../styles/gallery.module.css";
 import Footer from "../components/footer/footer";
-
+import Head from 'next/head'
 
 
 export default function Gallery() {
   return (
     
     <div className={styles.gallery} >
+      <Head>
+        <title>Gallery - Cyber Battles</title>
+        <meta name="description" content="Created by Cyber Battles" />
+        <link rel="icon" href="/favicon.ico" />
+                
+      </Head>
         <StickyHeader />
         <Gallery1 />
         <Gallery2 />
